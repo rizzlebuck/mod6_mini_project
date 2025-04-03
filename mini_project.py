f7ee5a3e3c6332e043e48fdfb0fd89c41288a2cc
rizzlebuck <aranda.nicholasb@gmail.com>
2025-04-03 15:26:40 -0700

    mod6_mini

---
+++ mini_project.py
@@ -0,0 +1,215 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from password import my_password
+from marshmallow import fields
+from marshmallow import ValidationError
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://root:{my_password}@localhost/mod6_mini_project'
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+# Many-to-Many relationship table
+order_product = db.Table('Order_Product',
+    db.Column('order_id', db.Integer, db.ForeignKey('Orders.id'), primary_key=True),
+    db.Column('product_id', db.Integer, db.ForeignKey('Products.id'), primary_key=True)
+)
+
+
+class Customer(db.Model):
+    __tablename__ = 'Customers'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(320))
+    phone = db.Column(db.String(15))
+    orders = db.relationship('Order', backref='customer')
+
+
+class Order(db.Model):
+    __tablename__ = 'Orders'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('Customers.id'))
+    products = db.relationship('Product', secondary=order_product, backref=db.backref('orders'))
+
+
+class Product(db.Model):
+    __tablename__ = 'Products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    stock = db.Column(db.Integer, default=0, nullable=False)
+    orders = db.relationship('Order', secondary=order_product, backref=db.backref('products'))
+
+
+class CustomerAccount(db.Model):
+    __tablename__ = 'Customer_Accounts'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('Customers.id'))
+    customer = db.relationship('Customer', backref='customer_account', uselist=False)
+
+
+class CustomerSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Customer
+        include_fk = True
+
+
+class CustomerAccountSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = CustomerAccount
+        include_fk = True
+
+
+class ProductSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Product
+        include_fk = True
+
+
+class OrderSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Order
+        include_fk = True
+
+
+# Initialize schemas
+customer_schema = CustomerSchema()
+customers_schema = CustomerSchema(many=True)
+customer_account_schema = CustomerAccountSchema()
+customer_accounts_schema = CustomerAccountSchema(many=True)
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
+order_schema = OrderSchema()
+orders_schema = OrderSchema(many=True)
+
+
+@app.route('/customers', methods=['GET'])
+def get_customers():
+    customers = Customer.query.all()
+    return customers_schema.jsonify(customers)
+
+
+@app.route('/customers', methods=['POST'])
+def add_customer():
+    try:
+        customer_data = customer_schema.load(request.json)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+
+    new_customer = Customer(
+        name=customer_data['name'],
+        email=customer_data['email'],
+        phone=customer_data['phone']
+    )
+    db.session.add(new_customer)
+    db.session.commit()
+    return jsonify({"message": "New customer added successfully"})
+
+
+@app.route('/customers/<int:id>', methods=['PUT'])
+def update_customer(id):
+    customer = Customer.query.get_or_404(id)
+    try:
+        customer_data = customer_schema.load(request.json)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+
+    customer.name = customer_data['name']
+    customer.email = customer_data['email']
+    customer.phone = customer_data['phone']
+    db.session.commit()
+    return jsonify({"message": "Customer details updated successfully"})
+
+
+@app.route('/customers/<int:id>', methods=['DELETE'])
+def delete_customer(id):
+    customer = Customer.query.get_or_404(id)
+    db.session.delete(customer)
+    db.session.commit()
+    return jsonify({"message": "Customer removed successfully"})
+
+
+@app.route('/products', methods=['GET'])
+def get_products():
+    products = Product.query.all()
+    return products_schema.jsonify(products)
+
+
+@app.route('/products', methods=['POST'])
+def add_product():
+    try:
+        product_data = product_schema.load(request.json)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+
+    new_product = Product(
+        name=product_data['name'],
+        price=product_data['price'],
+        stock=product_data['stock']
+    )
+    db.session.add(new_product)
+    db.session.commit()
+    return jsonify({"message": "New product added successfully"})
+
+
+@app.route('/products/<int:id>', methods=['PUT'])
+def update_product(id):
+    product = Product.query.get_or_404(id)
+    try:
+        product_data = product_schema.load(request.json)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+
+    product.name = product_data['name']
+    product.price = product_data['price']
+    product.stock = product_data['stock']
+    db.session.commit()
+    return jsonify({"message": "Product details updated successfully"})
+
+@app.route('/products/<int:id>', methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get_or_404(id)
+    db.session.delete(product)
+    db.session.commit()
+    return jsonify({"message": "Product removed successfully"})
+
+
+@app.route('/orders', methods=['POST'])
+def place_order():
+    try:
+        order_data = order_schema.load(request.json)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+
+    new_order = Order(
+        customer_id=order_data['customer_id'],
+        date=order_data['date']
+    )
+    db.session.add(new_order)
+    db.session.commit()
+
+    # Add products to the order
+    for product_id in order_data.get('products', []):
+        product = Product.query.get(product_id)
+        if product:
+            new_order.products.append(product)
+    db.session.commit()
+
+    return jsonify({"message": "Order placed successfully"})
+
+
+@app.route('/orders/<int:id>', methods=['GET'])
+def get_order(id):
+    order = Order.query.get_or_404(id)
+    return order_schema.jsonify(order)
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
